b35b4ce34dd576581368848b3e3e6abddcf339ba
BlackBow393 <t9374bow86@outlook.com>
2025-03-06 22:04:20 +0900

    ファイルパスをソースコードに設定

--- BI_Refresh.py
+++ BI_Refresh.py
@@ -4,37 +4,38 @@ import time
 
 from pywinauto import Desktop, Application, keyboard
 
-def main(workbook):
+def main():
     exe = 'PBIDesktop.exe'
+
+    # Power BIのファイルパスを直接指定
+    workbook = r"C:\Users\t9374\OneDrive\デスクトップ\個人家計簿BI\個人家計簿_ver.1.0.0.pbix"  # ここにPower BIファイルのパスを指定
 
     # ファイルを開く
     os.system('start "" "{0}"'.format(workbook))
     app = Application(backend='uia').connect(path=exe)
-    time.sleep(60)
+    time.sleep(30)
     try:
         # Windowを指定
-        win = app.window(title_re = '.*Power BI Desktop')
+        win = app.window(title_re = r'^個人家計簿_ver\.1\.0\.0')
         win.set_focus()
         # ホーム＞更新をクリック
         win.ホーム.wait("visible")
         win.ホーム.click_input()
         win.更新.wait("visible")
         win.更新.click_input()
-        win.キャンセル.wait_not("visible",timeout=6000)
+        win.キャンセル.wait_not("visible", timeout=6000)
         # 保存
         keyboard.send_keys("^s")
         time.sleep(120)
+
+        # 更新完了メッセージ
+        print("更新完了")
     except Exception as e:
+        print("更新できませんでした")
         print(e)
     finally:
         app.kill()
 
 
 if __name__ == '__main__':
-    try:
-        file_path = sys.argv[1]
-    except (IndexError):
-        print('ファイルを指定してください。')
-        sys.exit()
-
-    main(file_path)
+    main()
