11575b3c4a5b949438b864a2e8b81fd1d3d5f2f4
BlackBow393 <t9374bow86@outlook.com>
2025-03-07 22:24:17 +0900

    待機時間の指定方法を変更

--- BI_Refresh.py
+++ BI_Refresh.py
@@ -1,8 +1,6 @@
 import os
-import sys
 import time
-
-from pywinauto import Desktop, Application, keyboard
+from pywinauto import Application, keyboard
 
 def main():
     exe = 'PBIDesktop.exe'
@@ -12,30 +10,40 @@ def main():
 
     # ファイルを開く
     os.system('start "" "{0}"'.format(workbook))
-    app = Application(backend='uia').connect(path=exe)
-    time.sleep(30)
+    
     try:
-        # Windowを指定
-        win = app.window(title_re = r'^個人家計簿_ver\.1\.0\.0')
+        # アプリケーションに接続
+        app = Application(backend='uia').connect(path=exe)
+        
+        # ウィンドウが表示されるまで待つ
+        win = app.window(title_re=r'^個人家計簿_ver\.1\.0\.0')
+        win.wait('visible', timeout=30)  # 30秒間、ウィンドウが表示されるのを待機
+
+        # ウィンドウにフォーカスを当てる
         win.set_focus()
+
         # ホーム＞更新をクリック
-        win.ホーム.wait("visible")
+        win.ホーム.wait('visible')
         win.ホーム.click_input()
-        win.更新.wait("visible")
+        
+        win.更新.wait('visible')
         win.更新.click_input()
-        win.キャンセル.wait_not("visible", timeout=6000)
+
+        # キャンセルボタンが非表示になるのを待つ（最大15秒）
+        win.キャンセル.wait_not('visible', timeout=15000)
+
         # 保存
         keyboard.send_keys("^s")
-        time.sleep(120)
+        time.sleep(15)
 
         # 更新完了メッセージ
         print("更新完了")
+        
     except Exception as e:
         print("更新できませんでした")
         print(e)
     finally:
         app.kill()
-
 
 if __name__ == '__main__':
     main()
